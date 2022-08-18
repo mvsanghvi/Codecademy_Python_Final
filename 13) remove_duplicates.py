81865164a656bac9e6d1d6a9f5379e8f67fb40b0
mvsanghvi <mevsanghvi@gmail.com>
2022-08-18 14:52:26 -0500

    Redo Code Commit

--- 13) remove_duplicates.py
+++ 13) remove_duplicates.py
@@ -1,16 +1,25 @@
+#Using only commands and data storage collections taught in Codecademy
 def remove_duplicates(lst):
   print(lst)
-  freq= {}
-  for x in lst:
-    print(x)
-    if (x in freq):
-      freq[x] += 1
-      #print x
-      print(freq[x])
-      # if freq[x] > 1:
-        # lst.remove(x)
-        # print lst
+  new_sequence= []
+  for i in lst:
+    if i not in new_sequence:
+      new_sequence.append(i)
     else:
-      freq[x] = 1
-    print(freq)
-  return list(freq.keys())
+      continue
+  print(new_sequence)
+  return new_sequence
+
+#Using sets, which not covered in Codecademy lessons
+# def remove_duplicates(lst):
+#   print(lst)
+#   freq= {}
+#   for x in lst:
+#     print(x)
+#     if (x in freq):
+#       freq[x] += 1
+#       print(freq[x])
+#     else:
+#       freq[x] = 1
+#     print(freq)
+#   return list(freq.keys())
