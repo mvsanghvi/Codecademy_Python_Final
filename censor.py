63c00de68c84cb8b9a4526ff9fa6a813c29d304b
mvsanghvi <mevsanghvi@gmail.com>
2022-08-18 11:34:22 -0500

    Rename Commit

--- censor.py
+++
@@ -1,7 +0,0 @@
-def censor(text, word):
-  print(word)
-  censored = '*' * len(word)
-  print(censored)
-  censored_text = text.replace(word, censored)
-  print(censored_text)
-  return censored_text
