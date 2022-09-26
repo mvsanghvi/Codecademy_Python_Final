84ad0401ef273c26499e5bfb37813f6bb874b1f2
mvsanghvi <mevsanghvi@gmail.com>
2022-09-26 12:21:08 -0500

    Addition Commit

--- Classes/Classes Overview.py
+++ Classes/Classes Overview.py
@@ -19,4 +19,22 @@ class Car(object):
 
 my_car= Car()
 
+# Classes can have member variables that store information about each class object. We call them member variables since they are information that belongs to the class object.
+# Creating member variables and assigning them initial values is as easy as creating any other variable:
 
+      # class ClassName(object):
+      #   memberVariable = "initialValue"
+# 3. Inside your Car class, replace the pass statement with a new member variable named condition and give it an initial value of the string "new".
+class Car(object):
+  condition= "new"
+
+my_car= Car()
+
+# Each class object we create has its own set of member variables. Since we’ve created an object my_car that is an instance of the Car class, my_car should already have a member variable named condition. 
+# This attribute gets assigned a value as soon as my_car is created.
+# 4. At the end of your code, use a print statement to display the condition of my_car.
+class Car(object):
+  condition= "new"
+
+my_car= Car()
+print(my_car.condition)
