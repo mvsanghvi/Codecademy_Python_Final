db26c8756dc3c9dbaca0da0672ee4fce45923786
mvsanghvi <mevsanghvi@gmail.com>
2022-11-10 11:47:13 -0600

    Solve Commit

--- File Input and or Output/8) DIY.py
+++ File Input and or Output/8) DIY.py
@@ -1,2 +1,4 @@
 # It worked! Our Python program successfully wrote to text.txt.
 # 1. Now you try: write any data you like to a file called text.txt using with…as. Give your file object the usual name: my_file.
+with open("text.txt", "w") as my_file:
+  my_file.write("Done.")
