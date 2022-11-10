bcefed14bd1420660e3668e0c4292d06128c362b
mvsanghvi <mevsanghvi@gmail.com>
2022-11-10 11:45:35 -0600

    Initial Commit

---
+++ File Input and or Output/8) DIY.py
@@ -0,0 +1,2 @@
+# It worked! Our Python program successfully wrote to text.txt.
+# 1. Now you try: write any data you like to a file called text.txt using with…as. Give your file object the usual name: my_file.
