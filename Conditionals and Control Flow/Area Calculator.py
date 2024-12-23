a50b0828abca50eb456fa69638303b7af86d8bfc
mvsanghvi <mevsanghvi@gmail.com>
2024-12-23 16:08:39 -0600

    Python 2 to 3 update

--- Conditionals and Control Flow/Area Calculator.py
+++ Conditionals and Control Flow/Area Calculator.py
@@ -3,17 +3,17 @@ This will
 calculate the area
 of either a circle or traingle!
 """
-print "The calculator is running"
+print("The calculator is running")
 option = raw_input("What shape are you calculating? Endter C for Cirle or T for Triangle: ")
 if option == 'C':
   radius = float(raw_input("Enter radius: "))
   areac= 3.14159*radius**2
-  print "Area = % f" % areac
+  print("Area = % f" % areac)
 elif option == 'T':
   base = float(raw_input("Enter base: "))
   height = float(raw_input("Enter height: "))
   areat= 0.5*base*height
-  print "Area = % f" % areat
+  print("Area = % f" % areat)
 else:
-  print "Invalid shape enterred"
-print "Exiting program. Goodbye :)"
+  print("Invalid shape enterred")
+print("Exiting program. Goodbye :)")
