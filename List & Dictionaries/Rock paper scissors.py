cc03b13bb42c35e281fce6be2be658ec4b8ba210
mvsanghvi <mevsanghvi@gmail.com>
2024-12-24 17:01:59 -0600

    Convert Python 2 to 3

--- List & Dictionaries/Rock paper scissors.py
+++ List & Dictionaries/Rock paper scissors.py
@@ -11,20 +11,20 @@ message = {
   "lost": "Aww you lost!"
 }
 def decide_winner(user_choice, computer_choice):
-  print "You selected: %s" % user_choice
-  print "We selected: %s" % computer_choice
+  print("You selected: %s") % user_choice
+  print("We selected: %s") % computer_choice
   if user_choice == computer_choice:
-    print message["tie"]
+    print(message["tie"])
   elif user_choice == options[0] and computer_choice == options[2]:
-    print message["won"]
+    print(message["won"])
   elif user_choice == options[1] and computer_choice == options[0]:
-    print message["won"]
+    print(message["won"])
   elif user_choice == options[2] and computer_choice == options[1]:
-    print message["won"]
+    print(message["won"])
   else:
-    print message["lost"]  
+    print(message["lost"]) 
 def play_RPS():
-  print "Rock, Paper, or Scissors?"
+  print("Rock, Paper, or Scissors?")
   user_choice = raw_input("Select Rock, Paper, or Scissors: ")
   user_choice = user_choice.upper()
   computer_choice = options[randint(0, 2)]
