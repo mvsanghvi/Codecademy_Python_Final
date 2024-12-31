1c53487e708a48b8bada81aea65c95e6b4a869e4
mvsanghvi <mevsanghvi@gmail.com>
2024-12-30 18:01:24 -0600

    Convert Python 2 to 3

--- Advanced Topics in Python/RGB_HEX Converter.py
+++ Advanced Topics in Python/RGB_HEX Converter.py
@@ -2,22 +2,22 @@ def rgb_hex():
   invalid_msg = "There is an error with the input"
   red = int(raw_input("Enter red (R) value: "))
   if (red < 0 or red > 255):
-    print invalid_msg
+    print(invalid_msg)
     return
   green = int(raw_input("Enter green (G) value: "))
   if (green < 0 or green > 255):
-    print invalid_msg
+    print(invalid_msg)
     return
   blue = int(raw_input("Enter blue (B) value: "))
   if (blue < 0 or blue > 255):
-    print invalid_msg
+    print(invalid_msg)
     return
   val = (red << 16) + (green << 8) + blue
-  print "%s" % (hex(val)[2:]).upper()
+  print("%s" % (hex(val)[2:]).upper())
 def hex_rgb():
   hex_val = raw_input("Enter the color (six hexadecimal digits): ")
   if len(hex_val) != 6:
-    print "Invalid hexidecimal value. Try again."
+    print("Invalid hexidecimal value. Try again.")
     return
   else:
     hex_val = int(hex_val, 16)
@@ -27,18 +27,18 @@ def hex_rgb():
   green = hex_val % two_hex_digits
   hex_val = hex_val >> 8
   red = hex_val % two_hex_digits
-  print "Red: %s Green: %s Blue: %s" % (red, green, blue)
+  print("Red: %s Green: %s Blue: %s" % (red, green, blue))
 def convert():
   while True:
     option = raw_input("Enter 1 to convert RGB to HEX. Enter 2 to convert HEX to RGB. Enter X to Exit: ")
     if option == '1':
-      print "RGB to Hex..."
+      print("RGB to Hex...")
       rgb_hex()
     elif option == '2':
-      print "Hex to RGB..."
+      print("Hex to RGB...")
       hex_rgb()
     elif option == 'X' or option == 'x':
       break
     else:
-      print "Error"
+      print("Error")
 convert()
