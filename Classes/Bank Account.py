c5959a14f7117daa96f8235589b0ff54739fddee
mvsanghvi <mevsanghvi@gmail.com>
2025-01-02 17:50:45 -0600

    Convert Python 2 to 3

--- Classes/Bank Account.py
+++ Classes/Bank Account.py
@@ -5,26 +5,26 @@ class BankAccount(object):
   def __repr__(self):
     return "%s's account. Balance: $%.2f" % (self.name, self.balance)
   def show_balance(self):
-    print "Total balance is $%.2f" % self.balance
+    print("Total balance is $%.2f" % self.balance)
   def deposit(self, amount):
     if amount <= 0:
-      print "Deposit needs to be positive input"
+      print("Deposit needs to be positive input")
       return
     else:
-      print "Depositing $%.2f" % amount
+      print("Depositing $%.2f" % amount)
       self.balance += amount
       self.show_balance()
   def withdraw(self, amount):
     if amount > self.balance:
-      print "Not enough money in account"
+      print("Not enough money in account")
       return
     else:
-      print "Withdrawing $%.2f" % amount
+      print("Withdrawing $%.2f" % amount)
       self.balance -= amount
       self.show_balance()
 my_account = BankAccount("Megan")
-print my_account
+print(my_account)
 my_account.show_balance()
 my_account.deposit(2000)
 my_account.withdraw(1000)
-print my_account
+print(my_account)
