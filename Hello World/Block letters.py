8ff856a9c2ae36774d1ba0fe6a4237cc8065b2b1
mvsanghvi <mevsanghvi@gmail.com>
2025-01-03 21:33:07 -0600

    New Project

---
+++ Hello World/Block letters.py
@@ -0,0 +1,20 @@
+#Megan
+#I'm an only child
+b1= """
+       M   M
+       MM MM
+       MM MM
+       M M M
+       M   M
+       M   M
+       M   M"""
+print(b1)
+b2="""
+        SSS 
+       S   S
+       S    
+        SSS 
+           S
+       S   S
+        SSS """
+print(b2) 
